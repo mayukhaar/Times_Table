b7d5f07bab9972d595770059f2f3e39234368d09
mayukhaar <73047883+mayukhaar@users.noreply.github.com>
2021-01-08 23:00:54 -0500

    New commit

---
+++ Multiplication Game.py
@@ -0,0 +1,68 @@
+print("MULTIPLICATION GAME:")
+print("*********************")
+
+print("9 x 1 =")
+answer_1 = input("Answer:")
+if answer_1 == "9" or answer_1 == "nine" or answer_1 == "Nine":
+    print("Correct!")
+    print("8 x 2 =")
+    answer_2 = input("Answer:")
+    if answer_2 == "16" or answer_2 == "sixteen" or answer_2 == "Sixteen":
+        print("Correct!")
+        print("5 x 5 =")
+        answer_3 = input("Answer:")
+        if answer_3 == "25" or answer_3 == "twenty five" or answer_3 == "Twenty five" or answer_3 == "twenty-five" or answer_3 == "Twenty-five":
+            print("Correct!")
+            print("7 x 3 =")
+            answer_4 = input("Answer:")
+            if answer_4 == "21" or answer_4 == "twenty one" or answer_4 == "Twenty one" or answer_4 == "twenty-one" or answer_4 == "Twenty-one":
+                print("Correct!")
+                print("2 x 6 =")
+                answer_5 = input("Answer:")
+                if answer_5 == "12" or answer_5 == "twelve" or answer_5 == "Twelve":
+                    print("Correct!")
+                    print("4 x 3 =")
+                    answer_6 = input("Answer:")
+                    if answer_6 == "12" or answer_6 == "twelve" or answer_6 == "Twelve":
+                        print("Correct!")
+                        print("8 x 9 =")
+                        answer_7 = input("Answer:")
+                        if answer_7 == "72" or answer_7 == "seventy two" or answer_7 == "Seventy two" or answer_7 == "seventy-two" or answer_7 == "Seventy-two":
+                            print("Correct!")
+                            print("3 x 10 =")
+                            answer_8 = input("Answer:")
+                            if answer_8 == "30" or answer_8 == "thirty" or answer_8 == "Thirty":
+                                print("Correct!")
+                                print("5 x 4 =")
+                                answer_9 = input("Answer:")
+                                if answer_9 == "20" or answer_9 == "twenty" or answer_9 == "Twenty":
+                                    print("Correct!")
+                                    print("10 x 10 =")
+                                    answer_10 = input("Answer:")
+                                    if answer_9 == "100" or answer_9 == "one hundred" or answer_9 == "One hundred":
+                                        print("Correct!")
+                                    else:
+                                        print("Sorry, I don't understand that response, try again.")
+                                else:
+                                    print("Sorry, I don't understand that response, try again.")
+                            else:
+                                print("Sorry, I don't understand that response, try again.")
+                        else:
+                            print("Sorry, I don't understand that response, try again.")
+                    else:
+                        print("Sorry, I don't understand that response, try again.")
+                else:
+                    print("Sorry, I don't understand that response, try again.")
+            else:
+                print("Sorry, I don't understand that response, try again.")
+        else:
+            print("Sorry, I don't understand that response, try again.")
+    else:
+        print("Sorry, I don't understand that response, try again.")
+else:
+    print("Sorry, I don't understand that response, try again.")
+    #break
+#question_2 == print("")
+
+
+
